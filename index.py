41968044ac98b1c65f10133207f7512e7200ac1a
eysemberth <eysemberth.abarca@gmail.com>
2022-09-20 22:14:21 -0400

    pagina institucion

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 from app import app, db
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:tCuxKBXocFBcxwFxHmgk@containers-us-west-90.railway.app:7029/railway'
+
+db.init_app(app)
+
 if __name__ == '__main__':
     db.create_all()
     app.run('0.0.0.0', 5000, debug=True)
