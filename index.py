8baa07005d9189b5b89d164292cf02c7a5651228
eysemberth <eysemberth.abarca@gmail.com>
2022-09-20 22:20:51 -0400

    vercel database url

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 from app import app, db
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:tCuxKBXocFBcxwFxHmgk@containers-us-west-90.railway.app:7029/railway'
+# app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
-db.init_app(app)
+# db.init_app(app)
 
 if __name__ == '__main__':
     db.create_all()
